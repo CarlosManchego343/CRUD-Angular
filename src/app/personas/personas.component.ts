1e5b7e5cfff98a0bc615d4568018053c043ab053
Wendigo43 <carlosmariomanchego1@gmail.com>
2023-11-08 09:57:41 -0500

    Update-Event binding part 3-CMMP

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -8,9 +8,13 @@ import { Component } from "@angular/core";
 export class PersonasComponent {
     deshabilitar = false;
     mensaje: string = "No se a agregado ninguna persona"
-
+    titulo = ""
 
     agregarPersona() {
         this.mensaje = "Persona agregada"
+    }
+
+    modificarTitulo(event: Event) {
+        this.titulo = (<HTMLInputElement>event.target).value;
     }
 }
