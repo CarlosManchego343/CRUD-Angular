51e912a719c7a746b34cdbb830f7406af88d12e5
Wendigo43 <carlosmariomanchego1@gmail.com>
2023-12-04 08:52:11 -0500

    Feature-Person class-CMMP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// Se importa con el fin de hacer uso de Two way binding
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
---
+++ src/app/persona.model.ts
@@ -0,0 +1,13 @@
+export class Person {
+
+    // name: String = '';
+    // lastname: string = '';
+
+    // constructor(name: string, lastname: string) {
+    //     this.name = name;
+    //     this.lastname = lastname;
+    // }
+
+    constructor(public name: string, public lastname: string) {}
+
+}
